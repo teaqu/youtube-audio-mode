207e8d05dcd0aa58684be1314b35d5af3f9e3868
Calumks <calumks@outlook.com>
2018-09-09 17:03:38 -0400

    cleanup

--- ytaudio.user.js
+++ ytaudio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Youtube Audio Mode
 // @description Listen to only the audio on YouTube without loading the video.
-// @version     1.1
+// @version     1.1.1
 // @include     https://www.youtube.com/*
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @run-at      document-start
@@ -71,7 +71,6 @@
             let audioMode = ! await GM.getValue("ytAudioMode");
             this.setAttribute("aria-checked", audioMode);
             GM.setValue("ytAudioMode", audioMode);
-            let video = document.getElementsByTagName("video")[0];
 
             // Reload page to go back to video
             if ( ! audioMode) {
