9e1abceded435712aef739489e4600e85d8e8644
Calumks <calumks@outlook.com>
2018-09-16 21:16:23 -0400

    Code cleanup

--- ytaudio.user.js
+++ ytaudio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Youtube Audio Mode
 // @description Listen to only the audio on YouTube without loading the video.
-// @version     1.1.1
+// @version     1.2
 // @include     https://www.youtube.com/*
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @run-at      document-start
@@ -9,53 +9,43 @@
 // @grant       GM.setValue
 // @grant       GM.getValue
 // @noframes
-// @downloadURL  https://git.io/vhek9
+// @downloadURL https://git.io/vhek9
 // ==/UserScript==
 
+(function(open) {
+    window.addEventListener("yt-navigate-finish", audioMode);
+    window.onYouTubeIframeAPIReady = audioMode();
 
-(function(send) {
-
-    // Keep track of the current video
-    let videoId;
-
-    XMLHttpRequest.prototype.send = function() {
-
-        // Listen for audio requests
-        this.addEventListener("readystatechange", async function() {
-
-            // Add audio mode to the player's to menu
-            if (videoId != ytcsi.gt().info.docid) {
-                videoId = ytcsi.gt().info.docid;
-                addAudoModeToMenu();
-            }
-
+    async function audioMode() {
+        if (location.pathname == "/watch") {
             let video = document.getElementsByTagName("video")[0];
-
-            // Set audio
-            if (await GM.getValue("ytAudioMode", true)
-                && ! this.responseURL.includes("live=1")
-                && this.responseURL.includes("audio")
-                && ! video.src.includes('audio')
-                && videoId
-            ) {
-                video.pause();
-                video.src = this.responseURL.split("&range")[0];
-                video.play();
-
-                // Set poster image
-                setPoster(video, ["maxres", "sd", "hq"]);
+            let audioMode = await GM.getValue("ytAudioMode");
+            addToMenu(audioMode);
+            if (audioMode) {
+                setPoster(video, ["maxres", "hq", "sd"]);
+                watchStream(video);
             }
-        }, false);
-        send.apply(this, arguments);
-    };
+        }
+    }
+
+    // Watch the media streams so we can select the audio
+    function watchStream(video) {
+        XMLHttpRequest.prototype.open = function(method, url) {
+            let validStream = /^(?!.*live=1).+audio.+$/;
+            if (validStream.test(url) && ! video.src.includes("audio")) {
+                video.pause();
+                video.src = url.split("&range")[0];
+                video.play();
+            }
+            open.apply(this, arguments);
+        }
+    }
 
     // Add audio mode to the settings menu
-    async function addAudoModeToMenu() {
-        let audioMode = await GM.getValue("ytAudioMode", true);
+    async function addToMenu(audioMode) {
         let panel = document.getElementsByClassName("ytp-panel-menu")[0];
-        if (panel.innerHTML.includes("Audio Mode")) return;
-
-        panel.innerHTML += `
+        if (!panel.innerHTML.includes("Audio Mode")) {
+            panel.innerHTML += `
             <div class="ytp-menuitem"
                 aria-checked="${audioMode}"
                 id="audio-mode">
@@ -65,35 +55,31 @@
                 </div>
             </div>`;
 
-        // Toggle audio mode on or off
-        let audioToggle = document.getElementById("audio-mode");
-        audioToggle.onclick = async function() {
-            let audioMode = ! await GM.getValue("ytAudioMode");
-            this.setAttribute("aria-checked", audioMode);
-            GM.setValue("ytAudioMode", audioMode);
-
-            // Reload page to go back to video
-            if ( ! audioMode) {
-              location.reload();
+            // Toggle audio mode on or off
+            let audioToggle = document.getElementById("audio-mode");
+            audioToggle.onclick = async function() {
+                let audioMode = ! await GM.getValue("ytAudioMode");
+                this.setAttribute("aria-checked", audioMode);
+                GM.setValue("ytAudioMode", audioMode);
+                location.reload();
             }
         }
     }
 
     // Set the video poster from thumbnails with the best avaliable format
     // https://developers.google.com/youtube/v3/docs/thumbnails
-    function setPoster(video, fmts) {
+    async function setPoster(video, fmts) {
         let img = new Image();
+        let videoId = location.search.match(/v=(.+?)(&|$)/)[1];
         img.src = `//i.ytimg.com/vi/${videoId}/${fmts.shift()}default.jpg`
         img.onload = function() {
             // A height 90 is YouTube"s not found image.
             if (img.height <= 90) {
                 setPoster(video, fmts);
             } else {
-                // Background image used as poster does not work on edge with
-                // preload.
                 video.style.background = `url(${img.src}) no-repeat center`;
                 video.style.backgroundSize = "contain";
             }
         };
     }
-})(XMLHttpRequest.prototype.send);
+})(XMLHttpRequest.prototype.open);
